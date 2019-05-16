4fc0908d64bf45dc5eb04c695a9cfc7294b6a37e
Stephen Ficklin <spficklin@gmail.com>
2019-05-16 08:43:41 -0700

    Update Pfam.sh

--- terms/Pfam.sh
+++ terms/Pfam.sh
@@ -1,7 +1,6 @@
-wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/database_files/pfamA.txt.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/pfamA.txt.gz
 gunzip pfamA.txt.gz
-cat pfamA.txt | awk -F"\t" '{print "Pfam\t"$1"\t"$4}' > Pfam.terms.txt
+cat pfamA.txt | awk -F"\t" '{print "Pfam\t"$2"\t"$5}' > Pfam.terms.txt
 wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/database_files/dead_family.txt.gz
 gunzip dead_family.txt.gz
 cat dead_family.txt | awk -F"\t" '{print "Pfam\t"$1"\t"$2" (deprecated): "$3}' > Pfam.deprecated-terms.txt
-
