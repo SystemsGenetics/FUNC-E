2a4e29ab7ca1f0d24848ab38f0dc566792b24947
Stephen Ficklin <spficklin@gmail.com>
2019-06-20 18:30:41 -0700

    Update KEGG.sh

--- terms/KEGG.sh
+++ terms/KEGG.sh
@@ -1,12 +1,12 @@
-lynx "http://www.genome.jp/kegg-bin/download_htext?htext=ko00001.keg&format=htext&filedir="
+wget "http://www.genome.jp/kegg-bin/download_htext?htext=ko00001.keg&format=htext&filedir=" -O ko00001.keg
 
 # first get the list of pathways
-grep -P "^C" ko00001.keg > pathways.txt
-perl -pi -e 's/^C\s+(\d+)\s(.*?)\s*\[.*/KEGG\tko\1\t\2/' pathways.txt 
+grep -P "^C" ko00001.keg | grep "\[PATH:" | \
+perl -p -e 's/^C\s+(\d+)\s(.*?)\s*\[PATH:(.*)\].*/KEGG\t\3\t\2/' > pathways.txt 
 
 # second get the list of othologs
-egrep "K0|K1" ko00001.keg > orthologs.txt
-perl -pi -e 's/^D\s+(K\d+)\s+.*?;\s+(.*?)/KEGG\t\1\t\2/' orthologs.txt
+grep -P "^D" ko00001.keg | \
+perl -p -e 's/^D\s+(K\d+)\s+.*?;\s+(.*?)/KEGG\t\1\t\2/' > orthologs.txt
 
 cat pathways.txt orthologs.txt > KEGG.terms.txt
 
