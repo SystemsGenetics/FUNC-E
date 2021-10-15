d94eb06333dad87fae3d6b090a166495dfbc1ab2
Stephen Ficklin <spficklin@gmail.com>
2021-10-15 14:23:34 -0700

    updates to retreiving KEGG terms

--- terms/KEGG.sh
+++ terms/KEGG.sh
@@ -1,12 +1,16 @@
-wget "http://www.genome.jp/kegg-bin/download_htext?htext=ko00001.keg&format=htext&filedir=" -O ko00001.keg
 
-# first get the list of pathways
-grep -P "^C" ko00001.keg | grep "\[PATH:" | \
-perl -p -e 's/^C\s+(\d+)\s(.*?)\s*\[PATH:(.*)\].*/KEGG\t\3\t\2/' > pathways.txt 
+# Create a file of the KEGG orthologs
+wget http://rest.kegg.jp/list/ko 
+cat ko | awk -F"\t" '{print "KEGG\t"$1"\t"$2}' | perl -p -e 's/ko://' > KEGG_orthologs.terms.txt
 
-# second get the list of othologs
-grep -P "^D" ko00001.keg | \
-perl -p -e 's/^D\s+(K\d+)\s+.*?;\s+(.*?)/KEGG\t\1\t\2/' > orthologs.txt
+# Create a file of the KEGG pathways
+wget http://rest.kegg.jp/list/pathway
+cat pathway | awk -F"\t" '{print "KEGG\t"$1"\t"$2}' | perl -p -e 's/path:map/ko/' > KEGG_pathways.terms.txt
 
-cat pathways.txt orthologs.txt > KEGG.terms.txt
+# Create a file of the KEGG modules
+wget http://rest.kegg.jp/list/md
+cat md | awk -F"\t" '{print "KEGG\t"$1"\t"$2}' | perl -p -e 's/md://' > KEGG_modules.terms.txt
+
+cat KEGG_orthologs.terms.txt KEGG_pathways.terms.txt KEGG_modules.terms.txt > KEGG.terms.txt
+
 
