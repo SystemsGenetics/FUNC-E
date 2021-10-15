df5573e8d05595ddfeb9fa575cffd7ffca87eaa1
Stephen Ficklin <spficklin@gmail.com>
2021-10-15 14:34:38 -0700

    updated IPR terms file creation

--- terms/IPR.sh
+++ terms/IPR.sh
@@ -1,4 +1,3 @@
 wget ftp://ftp.ebi.ac.uk/pub/databases/interpro/entry.list
-perl -pi -e 's/^(IPR\d+)\s/IPR\t\1\t/' entry.list
-cat entry.list | grep IPR > IPR.terms.txt
+cat entry.list | awk -F"\t" '{print $1"\t"$2"\t"$4}' | grep IPR > IPR.terms.txt
 
