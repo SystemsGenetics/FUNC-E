6bc9e5954df3721f85f59a472129f57bfab79b08
Stephen Ficklin <spficklin@gmail.com>
2023-09-25 17:13:58 -0700

    Version change set

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='func_e',
     packages=find_packages(),
     url='https://systemsgenetics.github.io/FUNC-E/',
-    version='2.0.0',
+    version='2.0.1',
     description='FUNC-E is a python library and script for functional enrichment of gene lists. It follows a similar approach to that of DAVID (https://david.ncifcrf.gov/) in that it performs enrichment analysis using a Fisher\'s test but then clusters enriched annotations using Kappa Statistics. FUNC-E allows the user to provide their own annotation lists. It is fully executable on a UNIX command-line.',
     author='Ficklin and Feltus computational Labs (Washington State University & Clemson University)',
     license='GNU General Public License v3.0',
